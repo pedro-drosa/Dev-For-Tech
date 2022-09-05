e14d2421b80f669ed23d1a694f8702378aea35fc
pedro-drosa <pedro_drosa@hotmail.com>
2022-09-05 19:54:09 -0300

    feat: insert data

---
+++ mysql/02-insert-data.sql
@@ -0,0 +1,203 @@
+-- Inserindo dados em uma tabela
+
+-- INSERT INTO departamento (numero, nome, descricao) VALUES (1,'Tecnologia','Produtos para computadores');
+-- INSERT INTO departamento VALUES(null,'Eletrônicos', 'Tudo em eletrônicos');
+
+-- Inserindo vários registros em uma tabela
+
+-- INSERT INTO departamento VALUES 
+-- (null, 'Games', 'Para jogadores hard level'),
+-- (null, 'Acessórios','Cabos e conectores'),
+-- (null, 'Alimentação','Dev deve comer :D');
+
+-- INSERT INTO produto VALUES
+-- (null, 'PC Gamer', 'Computador cheio de led', 2500.0,3,'./img/pcgamer.png',1);
+-- SELECT * FROM departamento;
+
+-- Inserir informações na tabela clientes
+INSERT INTO cliente	VALUES 
+(null, 'Deena Yellowlea', 'dyellowlea0@vk.com', 'zk8i7xP', '52257-1203', '905326514-7'),
+(null, 'Mack Yellowlea', 'myellowlea1@wufoo.com', 'E5KTAQO', '56062-153', '024149964-X'),
+(null, 'Astrid Lewtey', 'alewtey2@newyorker.com', 'DI7ADAcln4mU', '49349-719', '111602870-0'),
+(null, 'Charlton Bunclark', 'cbunclark3@unblog.fr', 'HdLqbrHszi', '55154-4567', '583669691-8'),
+(null, 'Cirstoforo Pill', 'cpill4@twitter.com', 'Y1tvYzXfZQsy', '37000-826', '939162067-1'),
+(null, 'Tomi Salterne', 'tsalterne5@spotify.com', 'kJOMR0vlQLKE', '0472-1262', '291234905-2'),
+(null, 'Marylynne Isabell', 'misabell6@addthis.com', 'NjwHVc', '59779-989', '043489655-1'),
+(null, 'Jasen McGeever', 'jmcgeever7@ovh.net', 'uDBLIRSqz6V', '49483-272', '492086697-6'),
+(null, 'Ediva Itzkowicz', 'eitzkowicz8@wiley.com', 'dJesa41dMDrQ', '68026-650', '998169654-4'),
+(null, 'Pinchas Henriet', 'phenriet9@taobao.com', 'IGphAWSjCwP', '13668-107', '472202541-X'),
+(null, 'Farrah Keitley', 'fkeitleya@skype.com', '1b1ReAOmb', '63187-047', '825781792-9'),
+(null, 'Palmer Babb', 'pbabbb@virginia.edu', '40XO6FWsK', '54569-5532', '583886816-3'),
+(null, 'Mercie Roskams', 'mroskamsc@google.com.br', 'EQJKDSjOHgxV', '68016-002', '613301991-3'),
+(null, 'Barbey Tuvey', 'btuveyd@ca.gov', 'YFoEHGQi5', '52584-710', '831843184-7'),
+(null, 'Ulrika Kachel', 'ukachele@people.com.cn', 'TjLDbb', '0378-6993', '170672280-X'),
+(null, 'Alexi Ference', 'aferencef@people.com.cn', 'b8JlmOuy', '64735-080', '320659765-X'),
+(null, 'Florri Boch', 'fbochg@redcross.org', 'aSAmTuR', '36987-1248', '296454517-0'),
+(null, 'Celinka Kupke', 'ckupkeh@alexa.com', 'oL9w8TO', '55289-359', '484924020-8'),
+(null, 'Tristan Upchurch', 'tupchurchi@blogspot.com', 'bkXgFg', '0093-0292', '955010611-X'),
+(null, 'Hi Shenley', 'hshenleyj@prnewswire.com', 'sBpvRCvkK', '68645-211', '561667986-0'),
+(null, 'Hanan Lippiello', 'hlippiellok@posterous.com', 'OcR2efJf5T', '43857-0059', '163803651-9'),
+(null, 'Leupold Screen', 'lscreenl@nbcnews.com', 'NwuFxC1Nmi2', '36987-1011', '653755784-4'),
+(null, 'Daile Reyburn', 'dreyburnm@sourceforge.net', '6qBD9Ng', '46123-026', '167592229-2'),
+(null, 'Clemens Lafond', 'clafondn@tumblr.com', '6C3T8r', '49514-076', '722451024-6'),
+(null, 'Dorothy Besnard', 'dbesnardo@ning.com', '9pWSEApAnm', '62046-001', '036012776-2'),
+(null, 'Tamara Scapelhorn', 'tscapelhornp@example.com', 'Wuvrit3', '67544-507', '128707006-X'),
+(null, 'Reagan Conquest', 'rconquestq@columbia.edu', 'dZJ9YKazbtpG', '67226-1040', '136777748-8'),
+(null, 'Vasili Bussons', 'vbussonsr@twitpic.com', 'jo9XwwIrau', '52533-105', '446732830-0'),
+(null, 'Bridie Greatrex', 'bgreatrexs@phpbb.com', 'hlq72h5s', '54473-223', '361770569-3'),
+(null, 'Even De Cruce', 'edet@goodreads.com', 'fonFvTFf4x', '0781-5188', '781543873-3');
+
+SELECT * FROM cliente; -- Selecionar dados dos clientes
+
+-- Inserir informações na tabela departamento
+INSERT INTO departamento VALUES 
+(null, 'Movies', 'Switchable incremental support'),
+(null, 'Grocery', 'Synergistic contextually-based algorithm'),
+(null, 'Eletronics', 'Robust discrete protocol'),
+(null, 'Sports', 'Compatible web-enabled toolset'),
+(null, 'Industrial', 'Advanced homogeneous intranet');
+
+SELECT * FROM departamento; -- Selecionar dados dos departamentos
+
+-- Inserir informações na tabela de produtos
+INSERT INTO produto VALUES 
+(null, 'Warfarin Sodium', 'Total even-keeled knowledge base', 63, 49, 'http://dummyimage.com/100x100.png', 3),
+(null, 'fiber laxative', 'Distributed hybrid access', 69, 72, 'http://dummyimage.com/100x100.png', 4),
+(null, 'TetraStem', 'Proactive demand-driven success', 72, 95, 'http://dummyimage.com/100x100.png', 1),
+(null, 'FENTANYL CITRATE', 'Open-architected static task-force', 70, 59, 'http://dummyimage.com/100x100.png', 2),
+(null, 'Permethrin', 'Universal multimedia hub', 52, 90, 'http://dummyimage.com/100x100.png', 1),
+(null, 'harmon face values tussin cf', 'Multi-layered 3rd generation budgetary management', 22, 37, 'http://dummyimage.com/100x100.png', 5),
+(null, 'Medique CCP Caffeine Free', 'De-engineered bandwidth-monitored contingency', 94, 74, 'http://dummyimage.com/100x100.png', 2),
+(null, 'HYZAAR', 'Expanded analyzing leverage', 52, 24, 'http://dummyimage.com/100x100.png', 9),
+(null, 'Potassium Chloride in Dextrose and Sodium Chloride', 'Advanced 3rd generation model', 5, 89, 'http://dummyimage.com/100x100.png', 1),
+(null, 'Famotidine', 'Persevering eco-centric leverage', 25, 32, 'http://dummyimage.com/100x100.png/dddddd/000000', 1),
+(null, 'Premier Value Antacid and Pain Relief Medicine', 'Profit-focused incremental extranet', 83, 11, 'http://dummyimage.com/100x100.png', 4),
+(null, 'Nausea Constipation', 'Sharable eco-centric task-force', 34, 7, 'http://dummyimage.com/100x100.png', 3),
+(null, 'Minoxidil', 'Compatible tertiary synergy', 43, 26, 'http://dummyimage.com/100x100.png', 2),
+(null, 'good neighbor pharmacy nasal', 'Multi-layered maximized alliance', 97, 49, 'http://dummyimage.com/100x100.png', 5),
+(null, 'CELLBOOSTING HOMME', 'Decentralized systematic service-desk', 59, 15, 'http://dummyimage.com/100x100.png/dddddd/000000', 1),
+(null, 'Stool Softener DC Laxative', 'Re-engineered well-modulated array', 69, 50, 'http://dummyimage.com/100x100.png', 2),
+(null, 'LE TECHNIQ', 'Inverse object-oriented Graphic Interface', 86, 83, 'http://dummyimage.com/100x100.png', 3),
+(null, 'Antibacterial Skin Rain', 'Future-proofed composite pricing structure', 76, 95, 'http://dummyimage.com/100x100.png', 4),
+(null, 'Pyrite Salvia', 'Switchable fresh-thinking hierarchy', 26, 31, 'http://dummyimage.com/100x100.png', 5),
+(null, 'Kadian', 'Diverse holistic toolset', 9, 85, 'http://dummyimage.com/100x100.png', 1),
+(null, 'Pro-Care', 'User-friendly disintermediate architecture', 5, 66, 'http://dummyimage.com/100x100.png', 2),
+(null, 'Prevent', 'Organized dynamic forecast', 87, 78, 'http://dummyimage.com/100x100.png', 5),
+(null, 'Arnicare Arthritis', 'Right-sized directional process improvement', 53, 14, 'http://dummyimage.com/100x100.png', 3),
+(null, 'Treatment Set TS345049', 'Networked encompassing data-warehouse', 43, 48, 'http://dummyimage.com/100x100.png', 4),
+(null, 'Furosemide', 'Synchronised needs-based interface', 27, 58, 'http://dummyimage.com/100x100.png', 1),
+(null, 'Naproxen Sodium', 'Seamless grid-enabled hierarchy', 76, 70, 'http://dummyimage.com/100x100.png', 3),
+(null, 'IPKN HYDRO AND PORE BB 01 LIGHT', 'Automated dedicated paradigm', 36, 1, 'http://dummyimage.com/100x100.png', 2),
+(null, 'Irbesartan', 'Public-key bottom-line toolset', 33, 10, 'http://dummyimage.com/100x100.png', 4),
+(null, 'Instant Hand sanitizer', 'Ergonomic radical structure', 25, 1, 'http://dummyimage.com/100x100.png', 5),
+(null, 'Platinol', 'Proactive secondary knowledge base', 80, 40, 'http://dummyimage.com/100x100.png', 3);
+
+SELECT * FROM produto; --Selecionar informações dos produtos
+
+--Inserrir dodos na table endereco
+INSERT INTO endereco VALUES
+(null, 'Steel', 'Blackbird', '607', 'Structural and Misc Steel (Fabrication)', 'Trail', 'Natividade', null, '68788-9038', 29),
+(null, 'Brass', 'Holy Cross', '25', 'Site Furnishings', 'Pass', 'Klana', null, '63629-1639', 2),
+(null, 'Vinyl', 'Mockingbird', '5', 'Rebar & Wire Mesh Install', 'Drive', '‘Izrā', null, '57520-0575', 24),
+(null, 'Brass', 'Vidon', '64', 'Prefabricated Aluminum Metal Canopies', 'Park', 'Santa Monica', null, '68788-0285', 18),
+(null, 'Glass', 'Sundown', '1', 'Structural and Misc Steel (Fabrication)', 'Court', 'Eskilstuna', 'D', '42421-814', 11),
+(null, 'Steel', 'Debs', '6', 'Roofing (Asphalt)', 'Street', 'Přimda', null, '58411-128', 18),
+(null, 'Granite', 'Upham', '67', 'Glass & Glazing', 'Junction', 'Saint Paul', 'MN', '68151-1176', 18),
+(null, 'Brass', 'Anderson', '44', 'Glass & Glazing', 'Court', 'Los Angeles', 'SIN', '49643-005', 22),
+(null, 'Plexiglass', 'Pine View', '56993', 'Waterproofing & Caulking', 'Park', 'Mangarine', null, '34666-003', 13),
+(null, 'Steel', 'Dexter', '167', 'Ornamental Railings', 'Way', 'Skien', '08', '49288-0873', 10),
+(null, 'Wood', 'Vahlen', '722', 'Termite Control', 'Plaza', 'Cinyumput', null, '0268-6782', 24),
+(null, 'Aluminum', 'Farmco', '2', 'Prefabricated Aluminum Metal Canopies', 'Lane', 'Suzano', null, '60905-0029', 20),
+(null, 'Plexiglass', 'Coleman', '11660', 'Asphalt Paving', 'Circle', 'Burhānuddin', null, '10544-116', 17),
+(null, 'Plastic', 'Anniversary', '9964', 'Construction Clean and Final Clean', 'Terrace', 'Limeira', null, '50804-252', 27),
+(null, 'Vinyl', 'Scofield', '5508', 'Fire Protection', 'Trail', 'Erandique', null, '68828-198', 1),
+(null, 'Glass', 'Lien', '012', 'Temp Fencing, Decorative Fencing and Gates', 'Center', 'Wentai', null, '59886-413', 22),
+(null, 'Brass', 'Shopko', '9853', 'Drilled Shafts', 'Hill', 'Monte Alegre', null, '57520-0938', 1),
+(null, 'Wood', 'Sutherland', '57', 'Structural & Misc Steel Erection', 'Circle', 'Zlechov', null, '43526-115', 19),
+(null, 'Stone', 'Shopko', '183', 'Temp Fencing, Decorative Fencing and Gates', 'Pass', 'Semamung', null, '37000-345', 27),
+(null, 'Rubber', 'Armistice', '5', 'Masonry', 'Street', 'Brooklyn', 'NY', '20276-552', 23),
+(null, 'Aluminum', 'Knutson', '4323', 'Termite Control', 'Junction', 'Xarardheere', null, '68462-220', 26),
+(null, 'Plexiglass', 'Utah', '860', 'Plumbing & Medical Gas', 'Hill', 'Hanting', null, '0615-2525', 2),
+(null, 'Plastic', 'Grim', '9578', 'Ornamental Railings', 'Circle', 'Krajan Dermawuharjo', null, '0115-0522', 17),
+(null, 'Wood', 'Sauthoff', '6848', 'Masonry', 'Point', 'Khuzdār', null, '30142-166', 7),
+(null, 'Plastic', 'Holmberg', '4906', 'Site Furnishings', 'Park', 'La Huaca', null, '52685-442', 30),
+(null, 'Plastic', 'Shelley', '55998', 'Fire Sprinkler System', 'Circle', 'Boston', 'MA', '43063-059', 6),
+(null, 'Stone', 'Kenwood', '1', 'Hard Tile & Stone', 'Trail', 'Fenghuanglu', null, '64376-137', 27),
+(null, 'Stone', 'Garrison', '68567', 'Ornamental Railings', 'Crossing', 'Atlanta', 'GA', '30142-490', 3),
+(null, 'Granite', 'Schmedeman', '8875', 'Structural and Misc Steel (Fabrication)', 'Circle', 'Jiaocheng', null, '36987-2497', 30),
+(null, 'Aluminum', 'Anhalt', '854', 'Waterproofing & Caulking', 'Road', 'Tuojiang', null, '55319-041', 30);
+
+SELECT * FROM endereco; --Selecionar informações da tabela endereco
+
+--Inserir dados na tabela de pedidos
+INSERT INTO pedido VALUES
+(null, 'P', '2022-05-13', 445, 160, 78, 16),
+(null, 'A', '2022-01-03', 197, 333, 86, 2),
+(null, 'I', '2022-01-03', 218, 270, 89, 16),
+(null, 'C', '2022-06-17', 360, 151, 23, 15),
+(null, 'P', '2021-12-28', 268, 186, 100, 28),
+(null, 'R', '2022-05-28', 32, 46, 86, 2),
+(null, 'I', '2021-10-16', 113, 117, 90, 8),
+(null, 'I', '2021-10-02', 13, 142, 84, 8),
+(null, 'J', '2021-10-22', 310, 225, 72, 19),
+(null, 'C', '2022-02-06', 267, 258, 88, 11),
+(null, 'R', '2022-07-30', 348, 186, 64, 3),
+(null, 'P', '2022-02-20', 208, 126, 55, 14),
+(null, 'U', '2021-12-20', 298, 381, 57, 11),
+(null, 'R', '2022-05-30', 102, 120, 77, 1),
+(null, 'C', '2022-06-12', 387, 33, 55, 29),
+(null, 'P', '2022-04-16', 432, 373, 74, 6),
+(null, 'C', '2022-06-24', 171, 21, 87, 4),
+(null, 'C', '2022-04-16', 82, 259, 69, 28),
+(null, 'P', '2022-07-18', 72, 199, 69, 25),
+(null, 'P', '2022-02-21', 447, 489, 31, 23),
+(null, 'R', '2022-02-10', 441, 143, 86, 1),
+(null, 'I', '2021-12-30', 80, 119, 65, 5),
+(null, 'P', '2022-01-01', 225, 15, 13, 23),
+(null, 'M', '2021-10-10', 14, 349, 82, 5),
+(null, 'I', '2022-02-02', 361, 468, 49, 25),
+(null, 'L', '2021-11-02', 24, 449, 85, 2),
+(null, 'C', '2022-01-15', 252, 139, 40, 11),
+(null, 'C', '2021-11-21', 225, 324, 7, 7),
+(null, 'R', '2022-04-15', 275, 427, 69, 5),
+(null, 'P', '2022-04-27', 285, 20, 49, 9);
+
+SELECT * FROM 	pedido; --Selecionar informações dos pedidos
+
+--Inserir dados na tabela item_pedido
+INSERT INTO item_pedido VALUES 
+(null, 13, 24, 324, 40, 5),
+(null, 28, 55, 120, 39, 11),
+(null, 5, 58, 109, 50, 8),
+(null, 28, 24, 298, 48, 11),
+(null, 19, 92, 290, 43, 21),
+(null, 2, 47, 107, 50, 15),
+(null, 23, 77, 256, 36, 6),
+(null, 7, 79, 203, 47, 8),
+(null, 5, 14, 268, 61, 13),
+(null, 3, 82, 468, 39, 26),
+(null, 26, 12, 340, 48, 6),
+(null, 1, 92, 190, 37, 11),
+(null, 7, 13, 389, 62, 25),
+(null, 1, 38, 297, 55, 20),
+(null, 29, 96, 119, 57, 14),
+(null, 10, 96, 59, 56, 13),
+(null, 1, 78, 118, 44, 26),
+(null, 28, 23, 204, 54, 9),
+(null, 22, 96, 84, 55, 14),
+(null, 27, 71, 113, 47, 6),
+(null, 30, 70, 80, 41, 25),
+(null, 11, 17, 452, 34, 1),
+(null, 3, 53, 135, 35, 6),
+(null, 30, 74, 91, 45, 19),
+(null, 10, 67, 2, 60, 3),
+(null, 7, 93, 387, 45, 9),
+(null, 29, 91, 302, 62, 6),
+(null, 27, 10, 414, 46, 14),
+(null, 15, 40, 456, 59, 16),
+(null, 9, 5, 455, 48, 13);
+
+SELECT * from item_pedido; --Selecionar informações dos pedidos
+
+-- update departamento set nome="informática" WHERE numero = 1; -- Atualizar nome do departamento
+-- delete from departamento where numero = 5; -- Deletar o departamento 5
